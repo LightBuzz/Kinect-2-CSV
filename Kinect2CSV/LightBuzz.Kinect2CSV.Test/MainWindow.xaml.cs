32cb2d374c557f6b84e4da9606eda1b760a58ec4
Vangos <pterneas@gmail.com>
2014-10-26 22:24:40 +0200

    Initial commit

---
+++ Kinect2CSV/LightBuzz.Kinect2CSV.Test/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LightBuzz.Kinect2CSV.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor _sensor = null;
+        KinectCSVManager _recorder = null;
+        BodyFrameReader _reader = null;
+        IList<Body> _bodies = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _bodies = new Body[_sensor.BodyFrameSource.BodyCount];
+
+                _reader = _sensor.BodyFrameSource.OpenReader();
+                _reader.FrameArrived += BodyReader_FrameArrived;
+
+                _recorder = new KinectCSVManager();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (_reader != null)
+            {
+                _reader.Dispose();
+                _reader = null;
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+                _sensor = null;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (_recorder.IsRecording)
+            {
+                _recorder.Stop();
+
+                button.Content = "Start";
+
+                SaveFileDialog dialog = new SaveFileDialog
+                {
+                    Filter = "Excel files|*.csv"
+                };
+
+                dialog.ShowDialog();
+
+                if (!string.IsNullOrWhiteSpace(dialog.FileName))
+                {
+                    System.IO.File.Copy(_recorder.Result, dialog.FileName);
+                }
+            }
+            else
+            {
+                _recorder.Start();
+
+                button.Content = "Stop";
+            }
+        }
+
+        void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    frame.GetAndRefreshBodyData(_bodies);
+
+                    Body body = _bodies.Where(b => b != null && b.IsTracked).FirstOrDefault();
+
+                    if (body != null)
+                    {
+                        _recorder.Update(body);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Kinect2CSV/LightBuzz.Kinect2CSV/KinectCSVManager.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightBuzz.Kinect2CSV
+{
+    public class KinectCSVManager
+    {
+        int _current = 0;
+
+        bool _hasEnumeratedJoints = false;
+
+        public bool IsRecording { get; protected set; }
+
+        public string Folder { get; protected set; }
+
+        public string Result { get; protected set; }
+
+        public void Start()
+        {
+            IsRecording = true;
+            Folder = DateTime.Now.ToString("yyy_MM_dd_HH_mm_ss");
+
+            Directory.CreateDirectory(Folder);
+        }
+
+        public void Update(Body body)
+        {
+            if (!IsRecording) return;
+            if (body == null || !body.IsTracked) return;
+
+            string path = Path.Combine(Folder, _current.ToString() + ".line");
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                StringBuilder line = new StringBuilder();
+                
+                if (!_hasEnumeratedJoints)
+                {
+                    foreach (var joint in body.Joints.Values)
+                    {
+                        line.Append(string.Format("{0};;;", joint.JointType.ToString()));
+                    }
+                    line.AppendLine();
+
+                    foreach (var joint in body.Joints.Values)
+                    {
+                        line.Append("X;Y;Z;");
+                    }
+                    line.AppendLine();
+
+                    _hasEnumeratedJoints = true;
+                }
+
+                foreach (var joint in body.Joints.Values)
+                {
+                    line.Append(string.Format("{0};{1};{2};", joint.Position.X, joint.Position.Y, joint.Position.Z));
+                }
+
+                writer.Write(line);
+
+                _current++;
+            }
+        }
+
+        public void Stop()
+        {
+            IsRecording = false;
+
+            Result = DateTime.Now.ToString("yyy_MM_dd_HH_mm_ss") + ".csv";
+
+            using (StreamWriter writer = new StreamWriter(Result))
+            {
+                for (int index = 0; index < _current; index++)
+                {
+                    string path = Path.Combine(Folder, index.ToString() + ".line");
+
+                    if (File.Exists(path))
+                    {
+                        string line = string.Empty;
+
+                        using (StreamReader reader = new StreamReader(path))
+                        {
+                            line = reader.ReadToEnd();
+                        }
+
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+
+            Directory.Delete(Folder, true);
+        }
+    }
+}
